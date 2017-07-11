4c4c5b86a3486cad4d6b82a498c36cc1bb6acff2
qin <littelfei@users.noreply.github.com>
2017-07-11 17:12:02 +0800

    仿 中国机械设备网 首页-登录-注册

---
+++ cnjxsb/js/marquee.js
@@ -0,0 +1,50 @@
+function dmarquee(lineheight, speed, delay, id) {		
+	this.t; 
+	this.p = false; 
+	this.o = document.getElementById(id); 
+	this.h = this.o.innerHTML; 	
+	if(this.h.length < 10) return;
+	this.o.innerHTML = '<div id="'+id+'_tmp"><div>'+this.h+'</div></div>';	
+	var h1 = Number(document.getElementById(id).style.height.replace('px', '')); 
+	var h2 = document.getElementById(id+'_tmp').scrollHeight;
+	console.log("h1:"+h1+" h2:"+h2);
+	if(lineheight == -1) return;
+	if(h2*2 <= h1) { 
+		this.o.innerHTML = this.h; return; 
+	}else if(h2 >= h1) { 
+		this.o.innerHTML = this.h + this.h; 
+	}else { 
+		this.o.innerHTML = this.h + this.h + this.h;	
+	}
+	this.o.scrollTop = 0; 
+	var _this = this;
+
+	this.o.onmouseover = function() {_this.p = true;} 
+	this.o.onmouseout = function() {_this.p = false;}
+
+	this.start = function() { 
+		this.t = setInterval(function() {
+			_this.scrolling();
+		}, speed); 
+		if(!this.p) {this.o.scrollTop += 1;} 
+	} 
+	this.scrolling = function() { 		
+		if(this.o.scrollTop%lineheight !=0) { 
+			this.o.scrollTop += 1; 
+			if(this.o.scrollTop == h2) this.o.scrollTop = 0; 
+			
+		}else { 
+			clearInterval(this.t); 
+			setTimeout(function() {
+				_this.start();
+			}, delay); 
+		} 
+	}
+	setTimeout(function() {
+		_this.start();
+	}, delay);
+
+}
+//e.g. new dmarquee(20, 20, 1000, 'id'); id的高度必须要设定
+
+
